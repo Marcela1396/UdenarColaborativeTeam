1b5ae5cc180766f0e78ab647722a4af043d6faf8
cast <cast@port>
2018-04-08 23:20:01 -0500

    Avances menores

--- SistemaMaquinaExpendedora/src/MaquinaExpendedora/MaquinaExpendedora.java
+++ SistemaMaquinaExpendedora/src/MaquinaExpendedora/MaquinaExpendedora.java
@@ -1,20 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package MaquinaExpendedora;
 
 /**
  *
- * @author cast
+ * @author JuanPablo
  */
 public class MaquinaExpendedora
 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args)
     {        
         System.out.println("Inicio");
